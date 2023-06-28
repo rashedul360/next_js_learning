abf1f34c57d12dfcad270d0f6debd65d689e3fab
Rashedul islam <rashedul360rs@gmail.com>
2023-06-28 16:18:01 +0600

    next config test

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
  devIndicators: {
   buildActivity: false,
  },
- distDir: "build/deep_learning",
+
  productionBrowserSourceMaps: true,
  images: {
   deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
